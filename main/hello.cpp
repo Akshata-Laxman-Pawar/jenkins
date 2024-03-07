839cba7501f69a157a9494697090a9796d78ba84
Akshata-Laxman-Pawar <126555727+Akshata-Laxman-Pawar@users.noreply.github.com>
2024-03-07 11:54:18 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,7 +5,9 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "PES1UG21CS061\n";
   cout << "I have successfully built and run\n";
+  cout << "My name is  Akshata\n";
+  cout << "My SRN is PES1UG21CS061\n";
+  cout << "this indicates the code is edited\n";
   return 0;
 }
